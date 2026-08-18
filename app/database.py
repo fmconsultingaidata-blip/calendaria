cac9a2bfb794741a98a510283f02f1deb44b7a49
fmconsultingaidata-blip <fmconsultingaidata@gmail.com>
2026-08-18 17:08:58 +0200

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # URL de votre base de données Supabase PostgreSQL
-DATABASE_URL = "postgresql://postgres.puaqtxuwoqwaozzgkcqo:Automatiserclavie@1998@aws-0-eu-west-2.pooler.supabase.com:6543/postgres"
+DATABASE_URL = "postgresql://postgres.puaqtxuwoqwaozzgkcqo:automaticerclavie@aws-0-eu-west-2.pooler.supabase.com:6543/postgres"
 
 # Note : connect_args a été retiré car il est spécifique à SQLite
 engine = create_engine(DATABASE_URL)
